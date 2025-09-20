dc3a19a933f424683c71ba726cbf2b91736ea6dc
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-09-20 15:31:40 -0500

    Added comments

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,9 +1,12 @@
+/**
+ * Display each order with price
+ * @returns each order and cost as a <p> element
+ */
 export const Orders = async () => {
     const response = await fetch("http://localhost:8088/order?_expand=paint&_expand=interior&_expand=technology&_expand=wheel")
 
     if (response.ok) {
         const orders = await response.json()
-        console.log(orders)
         
         let html = ``
         orders.forEach((order) => {

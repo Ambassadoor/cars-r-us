d2db2cae9dc679dee19afec6329a8e3650ddd445
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-09-20 10:51:10 -0500

    Created selection component

---
+++ scripts/Selection.js
@@ -0,0 +1,26 @@
+// Return a select element for the given source
+
+/**
+ * Creates a select element with options from the provided source
+ * @param {*} source A database table name
+ * @returns A select element with options
+ */
+export const Selection = async (source) => {
+    // Fetch the provided table
+    const response = await fetch(`http://localhost:8088/${source}`) 
+
+    // Create basic select element
+    let html = `<select id="${source}><option value="0">Select an option...</option>`
+
+    // Add options on successful fetch
+    if (response.ok) {
+        const selections = await response.json()
+        selections.forEach(selection => {
+            html += `<option value=${selection.id}>${selection.name}${selection.detail? ` (${selection.detail})` : ""}</option>`
+        });
+
+    }
+    html += `</select>`
+
+    return html
+}

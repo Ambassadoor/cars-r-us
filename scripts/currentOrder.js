aeaf8d46a69faea350176247a627e042e7bbd6a9
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-09-20 15:01:26 -0500

    Added rerender & state reset functionality

--- scripts/currentOrder.js
+++ scripts/currentOrder.js
@@ -1,3 +1,5 @@
+import {render} from "./main.js"
+
 const currentOrder = {
     paintId: null,
     interiorId: null,
@@ -11,22 +13,25 @@ export const onSelect = (e) => {
     const key = target.id
 
     currentOrder[key] = value
+
 }
 
 export const onSubmit = async () => {
     if (!Object.values(currentOrder).some(value => value === null || value === 0) ) {
-            currentOrder.timestamp = Date.now()
+            const order = {...currentOrder, timestamp : Date.now()}
             const options = {
                 "method": "POST",
                 "headers": {
                 "Content-Type": "application/json"
                 }, 
-                "body": JSON.stringify(currentOrder)
+                "body": JSON.stringify(order)
             }
 
             const response = await fetch("http://localhost:8088/order", options)
             if (response.ok) {
                 console.log("Record added")
+                Object.keys(currentOrder).forEach(key => currentOrder[key] = null)
+                render()
             }
     } else {
         window.alert("Please select all options.")
